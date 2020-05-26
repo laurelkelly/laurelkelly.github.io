cd4537bd93375ce4814ca821bddd69c649517082
Laurel Kelly <52927436+laurelkelly@users.noreply.github.com>
2020-05-25 23:27:50 -0700

    Added gallery.html, css, js
    Created interactive NatureGram page and linked css and js.

---
+++ js/gallery.js
@@ -0,0 +1,42 @@
+new Swiper(".swiper-container", {
+    speed: 400,
+    spaceBetween: 100,
+    effect: "slide",
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+});
+
+// navigation physics
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.brand');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    ballXY.stop();
+  });
+
+  const { transformMap, smooth } = transform;
+
+  const smoothXY = transformMap({
+    x: smooth(300),
+    y: smooth(300)
+  });
+  
+  listen(ball, 'mousedown touchstart').start(() => 
+    schedule(everyFrame(), pointer(ballXY.get()))
+      .pipe(smoothXY)
+      .start(ballXY)
+  );
